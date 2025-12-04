6f4f8142b21b068a03a1be3488794d7476302ad7
RAnami156 <romaandreev414@gmail.com>
2025-12-04 21:32:55 +0100

    first commit

---
+++ scene/interface.gd
@@ -0,0 +1,6 @@
+extends CanvasLayer
+
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
---
+++ scene/menu.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/level.tscn")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
